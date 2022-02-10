63397d217d9c3d9392ea7c7f11e55db67e08c437
cddcx <84568016+cddcx@users.noreply.github.com>
2022-02-10 11:07:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,14 +21,14 @@ sed -i 's/bootstrap/argon/' feeds/luci/collections/luci/Makefile
 
 # 修改网络
 sed -i 's/eth0/eth0 eth2 eth3/' package/base-files/files/etc/board.d/99-default_network
-sed -i '2i # network config' package/emortal/default-settings/files/99-default-settings
-sed -i "3i uci set network.wan.proto='pppoe'" package/emortal/default-settings/files/99-default-settings
-sed -i "4i uci set network.wan.username='CD0283366379757'" package/emortal/default-settings/files/99-default-settings
-sed -i "5i uci set network.wan.password='19701115'" package/emortal/default-settings/files/99-default-settings
-sed -i "6i uci set network.wan.ifname='eth1'" package/emortal/default-settings/files/99-default-settings
-sed -i "7i uci set network.wan6.ifname='eth1'" package/emortal/default-settings/files/99-default-settings
-sed -i "8i uci set network.IPTV.proto='static'" package/emortal/default-settings/files/99-default-settings
-sed -i "9i uci set network.IPTV.ifname='eth1.43'" package/emortal/default-settings/files/99-default-settings
-sed -i "10i uci set network.IPTV.ipaddr='192.168.12.12'" package/emortal/default-settings/files/99-default-settings
-sed -i "11i uci set network.IPTV.netmask='255.255.255.0'" package/emortal/default-settings/files/99-default-settings
-sed -i '12i uci commit network' package/emortal/default-settings/files/99-default-settings
+sed -i '2i # network config' package/default-settings/files/zzz-default-settings
+sed -i "3i uci set network.wan.proto='pppoe'" package/default-settings/files/zzz-default-settings
+sed -i "4i uci set network.wan.username='CD0283366379757'" package/default-settings/files/zzz-default-settings
+sed -i "5i uci set network.wan.password='19701115'" package/default-settings/files/zzz-default-settings
+sed -i "6i uci set network.wan.ifname='eth1'" package/default-settings/files/zzz-default-settings
+sed -i "7i uci set network.wan6.ifname='eth1'" package/default-settings/files/zzz-default-settings
+sed -i "8i uci set network.IPTV.proto='static'" package/default-settings/files/zzz-default-settings
+sed -i "9i uci set network.IPTV.ifname='eth1.43'" package/default-settings/files/zzz-default-settings
+sed -i "10i uci set network.IPTV.ipaddr='192.168.12.12'" package/default-settings/files/zzz-default-settings
+sed -i "11i uci set network.IPTV.netmask='255.255.255.0'" package/default-settings/files/zzz-default-settings
+sed -i '12i uci commit network' package/default-settings/files/zzz-default-settings
